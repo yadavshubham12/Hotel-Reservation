b25e517b53c0d4a5b223ddbc5abe869f0d88ed04
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-02-07 12:18:13 +0530

    ADDED: Print to welcome Message

---
+++ src/com/blz/hotelReservation/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.blz.hotelReservation;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation Program");
+    }
+}
