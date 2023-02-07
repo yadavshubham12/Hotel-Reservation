5c700b2c3db1717bab4ceb45ce9329f851cb573f
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-02-07 13:45:52 +0530

    ADDED: add rating to each hotel

--- src/com/blz/hotelReservation/Hotel.java
+++ src/com/blz/hotelReservation/Hotel.java
@@ -6,22 +6,29 @@ import java.util.List;
             private String name;
             private double weekdayRate;
             private double weekendRate;
+            private int rating;
 
-            public Hotel(String name, double weekdayRate, double weekendRate) {
+            public Hotel(String name, double weekdayRate, double weekendRate, int rating) {
                 this.name = name;
                 this.weekdayRate = weekdayRate;
                 this.weekendRate = weekendRate;
+                this.rating = rating;
             }
+
             public String getName() {
                 return name;
             }
 
-            public double getRate(LocalDate date) {
-                if (date.getDayOfWeek().toString().equals("SATURDAY") || date.getDayOfWeek().toString().equals("SUNDAY")) {
-                    return weekendRate;
-                } else {
-                    return weekdayRate;
-                }
+            public double getWeekdayRate() {
+                return weekdayRate;
+            }
+
+            public double getWeekendRate() {
+                return weekendRate;
+            }
+
+            public int getRating() {
+                return rating;
             }
         }
 
@@ -36,25 +43,7 @@ class HotelReservationSystem {
         hotels.add(hotel);
     }
 
-    public List<Hotel> findCheapestHotels(LocalDate startDate, LocalDate endDate) {
-        List<Hotel> cheapestHotels = new ArrayList<>();
-        double cheapestRate = Double.MAX_VALUE;
-
-        for (Hotel hotel : hotels) {
-            double totalRate = 0.0;
-            for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
-                totalRate += hotel.getRate(date);
-            }
-
-            if (totalRate < cheapestRate) {
-                cheapestRate = totalRate;
-                cheapestHotels.clear();
-                cheapestHotels.add(hotel);
-            } else if (totalRate == cheapestRate) {
-                cheapestHotels.add(hotel);
-            }
-        }
-
-        return cheapestHotels;
+    public List<Hotel> getHotels() {
+        return hotels;
     }
 }
--- src/com/blz/hotelReservation/HotelReservation.java
+++ src/com/blz/hotelReservation/HotelReservation.java
@@ -7,20 +7,17 @@ public class HotelReservation {
 
     public static void main(String[] args) {
         HotelReservationSystem reservationSystem = new HotelReservationSystem();
-        reservationSystem.addHotel(new Hotel("Lakewood", 110, 90));
-        reservationSystem.addHotel(new Hotel("Bridgewood", 150, 50));
-        reservationSystem.addHotel(new Hotel("Ridgewood", 200, 150));
+        reservationSystem.addHotel(new Hotel("Lakewood", 110, 90, 3));
+        reservationSystem.addHotel(new Hotel("Bridgewood", 150, 50, 4));
+        reservationSystem.addHotel(new Hotel("Ridgewood", 200, 150, 5));
 
-        LocalDate startDate = LocalDate.of(2020, 9, 11);
-        LocalDate endDate = LocalDate.of(2020, 9, 12);
-
-        List<Hotel> cheapestHotels = reservationSystem.findCheapestHotels(startDate, endDate);
-        System.out.print("Cheapest Hotels: ");
-        for (Hotel hotel : cheapestHotels) {
-            System.out.print(hotel.getName() + " ");
+        List<Hotel> hotels = reservationSystem.getHotels();
+        for (Hotel hotel : hotels) {
+            System.out.println("Hotel: " + hotel.getName());
+            System.out.println("Weekday Rate: $" + hotel.getWeekdayRate());
+            System.out.println("Weekend Rate: $" + hotel.getWeekendRate());
+            System.out.println("Rating: " + hotel.getRating());
+            System.out.println();
         }
-        System.out.println();
-        System.out.println("Total Rates: $" + cheapestHotels.get(0).getRate(startDate) * (endDate.toEpochDay() - startDate.toEpochDay()));
-
     }
 }
