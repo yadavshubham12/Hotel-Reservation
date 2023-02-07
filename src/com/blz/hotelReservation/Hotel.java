ffea070e11c5adf9460fd9e7f525dca10d5969f5
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-02-07 13:17:20 +0530

    ADDED: added WeekendRates

--- src/com/blz/hotelReservation/Hotel.java
+++ src/com/blz/hotelReservation/Hotel.java
@@ -2,64 +2,57 @@ package com.blz.hotelReservation;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+        class Hotel {
+            private String name;
+            private double weekdayRate;
+            private double weekendRate;
 
-    class Hotel {
-        private String name;
-        private double regularRate;
-        private double rewardRate;
+            public Hotel(String name, double weekdayRate, double weekendRate) {
+                this.name = name;
+                this.weekdayRate = weekdayRate;
+                this.weekendRate = weekendRate;
+            }
 
-        public Hotel(String name, double regularRate, double rewardRate) {
-            this.name = name;
-            this.regularRate = regularRate;
-            this.rewardRate = rewardRate;
-        }
+            public String getName() {
+                return name;
+            }
 
-        public String getName() {
-            return name;
-        }
-
-        public double getRate(String customerType, LocalDate date) {
-            if (customerType.equals("Regular")) {
-                if (date.getDayOfWeek().toString().equals("FRIDAY") || date.getDayOfWeek().toString().equals("SATURDAY")) {
-                    return regularRate + 40;
+            public double getRate(LocalDate date) {
+                if (date.getDayOfWeek().toString().equals("SATURDAY") || date.getDayOfWeek().toString().equals("SUNDAY")) {
+                    return weekendRate;
                 } else {
-                    return regularRate;
-                }
-            } else if (customerType.equals("Reward")) {
-                return rewardRate;
-            } else {
-                return 0.0;
-            }
-        }
-    }
-
-    class HotelReservationSystem {
-        private List<Hotel> hotels;
-
-        public HotelReservationSystem() {
-            hotels = new ArrayList<>();
-        }
-
-        public void addHotel(Hotel hotel) {
-            hotels.add(hotel);
-        }
-
-        public Hotel findCheapestHotel(String customerType, LocalDate startDate, LocalDate endDate) {
-            Hotel cheapestHotel = hotels.get(0);
-            double cheapestRate = Double.MAX_VALUE;
-
-            for (Hotel hotel : hotels) {
-                double totalRate = 0.0;
-                for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
-                    totalRate += hotel.getRate(customerType, date);
-                }
-
-                if (totalRate < cheapestRate) {
-                    cheapestRate = totalRate;
-                    cheapestHotel = hotel;
+                    return weekdayRate;
                 }
             }
-
-            return cheapestHotel;
         }
-    }
+
+        class HotelReservationSystem {
+            private List<Hotel> hotels;
+
+            public HotelReservationSystem() {
+                hotels = new ArrayList<>();
+            }
+
+            public void addHotel(Hotel hotel) {
+                hotels.add(hotel);
+            }
+
+            public Hotel findCheapestHotel(LocalDate startDate, LocalDate endDate) {
+                Hotel cheapestHotel = hotels.get(0);
+                double cheapestRate = Double.MAX_VALUE;
+
+                for (Hotel hotel : hotels) {
+                    double totalRate = 0.0;
+                    for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
+                        totalRate += hotel.getRate(date);
+                    }
+
+                    if (totalRate < cheapestRate) {
+                        cheapestRate = totalRate;
+                        cheapestHotel = hotel;
+                    }
+                }
+
+                return cheapestHotel;
+            }
+        }
--- src/com/blz/hotelReservation/HotelReservation.java
+++ src/com/blz/hotelReservation/HotelReservation.java
@@ -3,19 +3,18 @@ package com.blz.hotelReservation;
 import java.time.LocalDate;
 
 public class HotelReservation {
-    private static Hotel cheapestHotel;
 
     public static void main(String[] args) {
         HotelReservationSystem reservationSystem = new HotelReservationSystem();
         reservationSystem.addHotel(new Hotel("Lakewood", 110, 90));
-        reservationSystem.addHotel(new Hotel("Bridgewood", 160, 60));
-        reservationSystem.addHotel(new Hotel("Ridgewood", 220, 150));
+        reservationSystem.addHotel(new Hotel("Bridgewood", 150, 50));
+        reservationSystem.addHotel(new Hotel("Ridgewood", 200, 150));
 
         LocalDate startDate = LocalDate.of(2020, 9, 10);
         LocalDate endDate = LocalDate.of(2020, 9, 11);
 
-        Hotel cheapestHotel = reservationSystem.findCheapestHotel("Regular", startDate, endDate);
+        Hotel cheapestHotel = reservationSystem.findCheapestHotel(startDate, endDate);
         System.out.println("Cheapest Hotel: " + cheapestHotel.getName());
-        System.out.println("Total Rates: $" + cheapestHotel.getRate("Regular", startDate) * (endDate.toEpochDay() - startDate.toEpochDay()));
+        System.out.println("Total Rates: $" + cheapestHotel.getRate(startDate) * (endDate.toEpochDay() - startDate.toEpochDay()));
     }
 }
